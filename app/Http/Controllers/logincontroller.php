c0a3e2e36eb77a79b89901dce75efe37f4003196
Alaa-en <85336732+Alaa-en@users.noreply.github.com>
2021-08-19 13:02:15 +0200

    login-voting

--- app/Http/Controllers/logincontroller.php
+++ app/Http/Controllers/logincontroller.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Guest_voting;
+use App\Models\Coins_voting;
 use Illuminate\Http\Request;
+use Exception;
 use Auth;
 class logincontroller extends Controller
 {
@@ -17,6 +19,27 @@ class logincontroller extends Controller
     if (Auth::attempt($credentials)) {
         if(Auth::user()->active  ){
             if(Auth::user()->type == 'user'){
+                $exists_coins =Guest_voting::where('ip_adress',$request->ip())->get();
+                if(count($exists_coins) > 0 ){
+                    foreach($exists_coins as $row){
+                        Guest_voting::where('id',$row->id)->delete();
+                        try{
+                            Coins_voting::create([
+                                'coin_id'=> $row->coin_id,
+                                'user_id'=> auth()->user()->id
+                            ]);
+                        }
+                        catch (exception $e) {
+                            \session()->flash('danger','you have voting before');
+
+                        }
+                       
+
+                    }
+                    
+                }
+
+
                     return redirect('/home');
             }
             elseif(Auth::user()->type == 'admin'){
